ee499fc3a52915b71cc1ac8df8a485cbff4babb8
wicky <wickyblog@gmail.com>
2014-03-01 22:05:52 +0800

    init chat room

--- wsgi/run.py
+++ wsgi/run.py
@@ -1,17 +1,35 @@
 import os
-from flask import Flask, render_template, request
+from flask import Flask, redirect, jsonify, render_template, request
+import redis
+import json
+import time
 
 app = Flask(__name__)
+
+rc = redis.Redis()
 
 @app.route('/')
 @app.route('/a')
 @app.route('/index')
 def index():
-    return 'oh yeah!!!!!!!!!!!!!!!!!!!!'
+    room_content = reversed(rc.zrevrange('chat_content', 0, 200, withscores=True))
+    room_content_list = []
+    for item in room_content:
+        room_content_list.append(json.loads(item[0]))
+    return render_template('room.html',room_content = room_content_list)
 
 @app.route('/hello')
 def fancy():
     return 'hello!!!'
+
+@app.route('/post_content', methods=['POST'])
+def post_content():
+    data = {
+            'content': request.form.get('content', ''),
+            'created': time.strftime('%m-%d %H:%M:%S'),
+            }
+    rc.zadd('chat_content', json.dumps(data), time.time())
+    return redirect('/')
 
 if __name__ == "__main__":
     app.run(debug = "True")
