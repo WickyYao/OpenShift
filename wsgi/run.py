fa326e50e541d71aa05b6d8f56df3a5024d00726
wicky <wickyblog@gmail.com>
2014-03-02 00:45:00 +0800

    test

--- wsgi/run.py
+++ wsgi/run.py
@@ -6,7 +6,7 @@ import time
 
 app = Flask(__name__)
 
-rc = redis.Redis(host= localhost)
+rc = redis.Redis(password: ZTNiMGM0NDI5OGZjMWMxNDlhZmJmNGM4OTk2ZmI5)
 ##rc = redis.Redis(host= 127.6.26.2, port= 16379, db=0)
 ##rc = redis.Redis()
 
